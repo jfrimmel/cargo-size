4e4aee534f1fc904041a082a727f365ee865cbb8
Julian Frimmel <julian.frimmel@gmail.com>
2019-01-05 20:53:05 +0100

    Converted to a library project.
    The whole crate is now a library, with a binary 'cargo-size', which uses
    the crate to provide the old functionality.

---
+++ src/bin/cargo-size.rs
@@ -0,0 +1,64 @@
+//! The actual binary, that uses the rust library.
+extern crate cargo_size;
+
+use cargo_size::binary;
+use cargo_size::change_directory;
+use cargo_size::error::Error;
+use cargo_size::memory_size;
+use cargo_size::mode::Mode;
+use colored::Colorize;
+use std::process;
+
+/// Try to execute the whole program or return at the first error.
+///
+/// On success, the function returns the program output.
+fn try_main() -> Result<String, Error> {
+    let mode = Mode::new();
+
+    change_directory()?;
+    mode.build_binary()?;
+    let binary = mode.binary()?;
+    let (code, data) = binary::read_size_from(&binary)?;
+
+    if let Some((code_memory, data_memory)) = memory_size() {
+        let code_percentage = code as f32 / code_memory as f32 * 100.0;
+        let data_percentage = data as f32 / data_memory as f32 * 100.0;
+        Ok(format!(
+            "Memory Usage
+             ------------
+             Program: {:>7} bytes ({:.1}% full)
+             Data:    {:>7} bytes ({:.1}% full)",
+            code, code_percentage, data, data_percentage
+        ))
+    } else {
+        Ok(format!(
+            "Memory Usage
+             ------------
+             Program: {:>7} bytes
+             Data:    {:>7} bytes",
+            code, data
+        ))
+    }
+}
+
+/// The program entry point.
+///
+/// This function executes `try_main()` and prints its result state. In case of
+/// success, the output is written to `stdout` with `Printing` in front of it to
+/// mimic the usual cargo behavior. The program exits with the status code `0`.
+///
+/// In case of an error, the error is printed to `stderr` prefixed with a red
+/// _Error_ in front of it to mimic the cargo behavior. The program exits with
+/// the status code `1`.
+fn main() {
+    match try_main() {
+        Ok(output) => {
+            println!("{:>12} {}", "Printing".bright_green().bold(), output);
+            process::exit(0);
+        }
+        Err(e) => {
+            eprintln!("{:>12} {}", "Error".bright_red().bold(), e);
+            process::exit(1);
+        }
+    }
+}
--- src/lib.rs
+++ src/lib.rs
@@ -25,52 +25,17 @@ extern crate elf;
 extern crate ldscript_parser;
 
 use crate::error::Error;
-use crate::mode::Mode;
-use colored::Colorize;
 use ldscript_parser::RootItem::Memory;
 use std::env;
 use std::fs;
-use std::process;
 
-mod binary;
-mod cargo;
-mod error;
-mod mode;
-
-/// Try to execute the whole program or return at the first error.
-///
-/// On success, the function returns the program output.
-fn try_main() -> Result<String, Error> {
-    let mode = Mode::new();
-
-    change_directory()?;
-    mode.build_binary()?;
-    let binary = mode.binary()?;
-    let (code, data) = binary::read_size_from(&binary)?;
-
-    if let Some((code_memory, data_memory)) = memory_size() {
-        let code_percentage = code as f32 / code_memory as f32 * 100.0;
-        let data_percentage = data as f32 / data_memory as f32 * 100.0;
-        Ok(format!(
-            "Memory Usage
-             ------------
-             Program: {:>7} bytes ({:.1}% full)
-             Data:    {:>7} bytes ({:.1}% full)",
-            code, code_percentage, data, data_percentage
-        ))
-    } else {
-        Ok(format!(
-            "Memory Usage
-             ------------
-             Program: {:>7} bytes
-             Data:    {:>7} bytes",
-            code, data
-        ))
-    }
-}
+pub mod binary;
+pub mod cargo;
+pub mod error;
+pub mod mode;
 
 /// Changes the current working directory to the crate root if possible.
-fn change_directory() -> Result<(), Error> {
+pub fn change_directory() -> Result<(), Error> {
     env::set_current_dir(cargo::root()?)?;
 
     Ok(())
@@ -82,7 +47,7 @@ fn change_directory() -> Result<(), Error> {
 /// If the file does not exist or has an invalid format, `None` is returned. To
 /// be valid, there have to be two sections present in the memory section, which
 /// are named `flash` and `ram` (case is ignored).
-fn memory_size() -> Option<(u64, u64)> {
+pub fn memory_size() -> Option<(u64, u64)> {
     fs::read_to_string("memory.x")
         .ok()
         .and_then(|content| ldscript_parser::parse(&content).ok())
@@ -113,26 +78,4 @@ fn memory_size() -> Option<(u64, u64)> {
                 None
             }
         })
-}
-
-/// The program entry point.
-///
-/// This function executes `try_main()` and prints its result state. In case of
-/// success, the output is written to `stdout` with `Printing` in front of it to
-/// mimic the usual cargo behavior. The program exits with the status code `0`.
-///
-/// In case of an error, the error is printed to `stderr` prefixed with a red
-/// _Error_ in front of it to mimic the cargo behavior. The program exits with
-/// the status code `1`.
-fn main() {
-    match try_main() {
-        Ok(output) => {
-            println!("{:>12} {}", "Printing".bright_green().bold(), output);
-            process::exit(0);
-        }
-        Err(e) => {
-            eprintln!("{:>12} {}", "Error".bright_red().bold(), e);
-            process::exit(1);
-        }
-    }
 }
